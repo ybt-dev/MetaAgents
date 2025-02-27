53396d095bdafaa63a2ecfdf1ed6e54c399bfdd8
ilya-tishonok <ilya.tishonok@satelliteinnovations.io>
2025-02-27 13:06:31 +0100

    remove shorten helper

--- frontend/src/components/WalletConnection/WalletConnection.tsx
+++ frontend/src/components/WalletConnection/WalletConnection.tsx
@@ -1,26 +1,22 @@
 import { useWallet } from '@initia/react-wallet-widget';
-import { shortenAddress } from '@/helpers/shorter.ts';
+import { truncate } from '@/utils';
 
 const WalletConnection = () => {
   const { address, onboard, view, account } = useWallet();
 
-  const handler = async () => {
-    try {
-      if (!account) {
-        onboard();
-      } else {
-        view();
-      }
-    } catch (e) {
-      console.log(e);
+  const handleConnectButtonClick = () => {
+    if (!account) {
+      onboard();
+    } else {
+      view();
     }
   };
 
   return (
     <div className="relative">
       <div className="w-28 rounded-md shadow-lg bg-gray-800 ring-1 ring-black ring-opacity-5 hover:bg-gray-700">
-        <button onClick={handler} className="w-full px-4 py-2 text-sm text-white">
-          {account ? shortenAddress(address) : 'Connect'}
+        <button onClick={handleConnectButtonClick} className="w-full px-4 py-2 text-sm text-white">
+          {account ? truncate(address) : 'Connect'}
         </button>
       </div>
     </div>
--- frontend/src/helpers/shorter.ts
+++
@@ -1,6 +0,0 @@
-export const shortenAddress = (str: string) => {
-  if (str.length <= 10) {
-    return str;
-  }
-  return str.slice(0, 5) + '...' + str.slice(-5);
-};
